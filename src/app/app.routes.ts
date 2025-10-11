bde976f71874a0421e94fd7addf3b748c7bc4087
ferd <beowulf1416@gmail.com>
2025-10-11 23:14:05 +0800

    add placeholder modules. modify user service.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,27 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        path: '',
-        loadComponent: () => import('./components/home/home').then(c => c.Home)
-    },
-    {
         path: 'user/sign-up',
         loadChildren: () => import('./modules/user-registration/user-registration-module').then(m => m.UserRegistrationModule)
     },
     {
         path: 'auth',
         loadChildren: () => import('./modules/authentication/authentication-module').then(m => m.AuthenticationModule)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./components/dashboard/dashboard').then(c => c.Dashboard)
+    },
+    {
+        path: 'tenant-admin',
+        loadChildren: () => import('./modules/tenant-admin/tenant-admin-module').then(m => m.TenantAdminModule)
+    },
+    {
+        path: 'admin',
+        loadChildren: () => import('./modules/admin/admin-module').then(m => m.AdminModule)
+    },
+    {
+        path: '',
+        loadComponent: () => import('./components/home/home').then(c => c.Home)
     }
 ];
--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,6 +1,10 @@
 import { Injectable, signal } from '@angular/core';
 
 import { User } from '../classes/user';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { ApiResponse } from '../classes/api-response';
+import { URL } from '../classes/urls';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +13,35 @@ export class UserService {
   public readonly current_user = signal(User.anonymous());
 
   constructor(
+    private http: HttpClient
   ) {}
+
+  sign_in(
+    email: string,
+    pw: string
+  ): Observable<ApiResponse> {
+    return this.http.post<ApiResponse>(
+      URL.session_sign_in,
+      {
+        email: email,
+        pw: pw
+      },
+      {
+        observe: 'response'
+      }
+    ).pipe(
+      map((value, index) => {
+        if (value.ok && value.headers.has('authorization')) {
+          let token = value.headers.get('authorization')?.replace('bearer', '')?.trim() || '';
+          console.debug('token', token);
+        }
+
+        return new ApiResponse(
+          false,
+          '//todo',
+          null
+        );
+      })
+    );
+  }
 }
