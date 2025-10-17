ed89c1b2fb6ee478471312859d0cc62ad0e01ae5
ferd <beowulf1416@gmail.com>
2025-10-17 14:11:18 +0800

    add admin layout component

--- src/app/modules/admin/admin-routing-module.ts
+++ src/app/modules/admin/admin-routing-module.ts
@@ -3,22 +3,30 @@ import { RouterModule, Routes } from '@angular/router';
 import { authorizedGuard } from '../../classes/guards/authorized-guard';
 import { PERMISSIONS } from '../../classes/permissions';
 
+import { Layout } from './components/layout/layout';
+
 const routes: Routes = [
   {
-    path: 'tenants',
-    title: 'Tenants',
-    loadComponent: () => import('./components/tenants/tenants').then(c => c.Tenants)
-  },
-  {
     path: '',
-    title: 'Admin Dashboard',
-    canActivate: [
-      authorizedGuard
-    ],
-    data: {
-      permission: PERMISSIONS.admin_dashboard_view
-    },
-    loadComponent: () => import('./components/dashboard/dashboard').then(c => c.Dashboard)
+    component: Layout,
+    children: [
+      {
+        path: 'tenants',
+        title: 'Tenants',
+        loadComponent: () => import('./components/tenants/tenants').then(c => c.Tenants)
+      },
+      {
+        path: '',
+        title: 'Admin Dashboard',
+        canActivate: [
+          authorizedGuard
+        ],
+        data: {
+          permission: PERMISSIONS.admin_dashboard_view
+        },
+        loadComponent: () => import('./components/dashboard/dashboard').then(c => c.Dashboard)
+      }
+    ]
   }
 ];
 
