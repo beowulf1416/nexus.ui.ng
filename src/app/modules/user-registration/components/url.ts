6fd33579d6619a0c57082b77cbc3ddc9446ae132
ferd <beowulf1416@gmail.com>
2025-10-18 21:49:19 +0800

    modify user registration module

--- src/app/modules/user-registration/components/url.ts
+++
@@ -1,6 +0,0 @@
-import { CONSTANTS } from "../../../classes/constants";
-
-export const URL = {
-    sign_up: CONSTANTS.api_base_url + '/api/v1/user/sign-up',
-    sign_up_verified: CONSTANTS.api_base_url + '/api/v1/user/sign-up-verified'
-};
