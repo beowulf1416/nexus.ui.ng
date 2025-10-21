e71015214d1f4ce5686a0f9b02ef9adbc60dd099
ferd <beowulf1416@gmail.com>
2025-10-21 15:41:46 +0800

    add placeholder components

---
+++ src/app/modules/admin/components/tenant/tenant.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tenant',
+  imports: [],
+  templateUrl: './tenant.html',
+  styleUrl: './tenant.css'
+})
+export class Tenant {
+
+}
