dd97c9a063b64997327ea7f73737928f6184e9a1
ferd <beowulf1416@gmail.com>
2025-11-02 10:40:18 +0800

    modify tenant and tenants components

--- src/app/modules/admin/modules/tenants/components/tenants/tenants.ts
+++ src/app/modules/admin/modules/tenants/components/tenants/tenants.ts
@@ -1,11 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-tenants',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './tenants.html',
   styleUrl: './tenants.css',
 })
 export class Tenants {
 
+  component = {
+    error: '',
+    formSearch: new FormGroup({
+      filter: new FormControl('', Validators.required)
+    })
+  };
+
+  constructor() {}
+
+
+  search(): void {
+    console.info('search');
+  }
 }
