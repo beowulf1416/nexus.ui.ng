752ab22ca3e31fca03929d78a3e8683c1dd51f72
ferd <beowulf1416@gmail.com>
2025-11-18 22:45:43 +0800

    add tenant_id field

--- src/app/classes/tenant.ts
+++ src/app/classes/tenant.ts
@@ -1,16 +1,20 @@
+import { v4 as uuidv4, NIL as NIL_UUID } from 'uuid';
+
 export class Tenant {
 
     constructor(
-        readonly name: string
+        public readonly tenant_id: string,
+        public readonly name: string
     ) {}
 
     static default(): Tenant {
         return new Tenant(
-            ''
+            NIL_UUID,
+            'default'
         );
     }
 
     get is_default() {
-        return this.name == '';
+        return this.tenant_id == NIL_UUID;
     }
 }
