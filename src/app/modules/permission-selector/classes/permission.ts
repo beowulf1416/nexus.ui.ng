972ad9f16c144017126585e2019eb5b7354c3798
ferd <beowulf1416@gmail.com>
2025-11-25 22:18:28 +0800

    modify permission selector

--- src/app/modules/permission-selector/classes/permission.ts
+++ src/app/modules/permission-selector/classes/permission.ts
@@ -1,2 +1,8 @@
 export class Permission {
+    
+    constructor(
+        readonly id: number,
+        readonly name: string,
+        readonly description: string
+    ) {}
 }
--- src/app/modules/permission-selector/components/permission-selector/permission-selector.ts
+++ src/app/modules/permission-selector/components/permission-selector/permission-selector.ts
@@ -1,5 +1,5 @@
-import { booleanAttribute, Component, input, output } from '@angular/core';
-import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { booleanAttribute, ChangeDetectorRef, Component, input, output } from '@angular/core';
+import { AbstractControl, FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Permission } from '../../classes/permission';
 import { PermissionService } from '../../services/permission-service';
 import { CommonModule } from '@angular/common';
@@ -8,6 +8,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ApiResponse } from '../../../../classes/api-response';
 
 @Component({
   selector: 'app-permission-selector',
@@ -26,7 +27,7 @@ import { MatInputModule } from '@angular/material/input';
 export class PermissionSelector {
 
   multiple = input(true, { transform: booleanAttribute });
-  permissions_selected = output<Array<Permission>>();
+  permissions_selected = output<Array<string>>();
 
   component = {
     error: '',
@@ -40,7 +41,8 @@ export class PermissionSelector {
   };
 
   constructor(
-    private ps: PermissionService
+    private ps: PermissionService,
+    private cd: ChangeDetectorRef
   ) {}
 
   get matches() {
@@ -49,5 +51,80 @@ export class PermissionSelector {
 
   get selected() {
     return this.component.formPermissionSelector.get('selected') as FormArray;
+  }
+
+  reset(): void {
+    console.info('filter');
+
+    this.component.formPermissionSelector.get('filters.filter')?.reset();
+  }
+
+  search(): void {
+    console.info('search');
+
+    let filter = this.component.formPermissionSelector.get('filters.filter')?.value || '%';
+
+    this.ps.fetch(
+      filter
+    ).subscribe({
+      next: (r: ApiResponse) => {
+        if(r.success) {
+          let permissions = (r.data as {
+            permissions: Array<Permission>
+          }).permissions;
+
+          let pfa = this.component.formPermissionSelector.get('matches') as FormArray;
+          pfa.clear();
+          permissions.forEach((p: Permission) => {
+            pfa.push(new FormGroup({
+              id: new FormControl(p.id, []),
+              name: new FormControl(p.name, []),
+              description: new FormControl(p.description, [])
+            }));
+          });
+          this.cd.detectChanges();
+        } else {
+          console.error('//todo', r);
+        }
+      },
+      error: (e) => {
+        console.error('//todo', e);
+      },
+      complete: () => {
+        console.info('//todo');
+      }
+    })
+  }
+
+  select(permission: Permission): void {
+    console.info('//todo', permission);
+
+    let sfa = this.component.formPermissionSelector.get('selected') as FormArray;
+    sfa.push(new FormGroup({
+      id: new FormControl(permission.id, []),
+      name: new FormControl(permission.name, []),
+      description: new FormControl(permission.description, [])
+    }));
+    this.cd.detectChanges();
+  }
+
+  deselect(permission: Permission, i: number): void {
+    console.info('//todo', permission);
+
+    let sfa = this.component.formPermissionSelector.get('selected') as FormArray;
+    sfa.removeAt(i);
+    this.cd.detectChanges();
+  }
+
+  permission_select(): void {
+    console.info('permission_select');
+
+    let sfa = this.component.formPermissionSelector.get('selected') as FormArray;
+    let selected_permission_ids = sfa?.controls
+      .map(c => c.value)
+      .map(c => c.id)
+    ;
+
+    this.permissions_selected.emit(selected_permission_ids);
   }
 }
