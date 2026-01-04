7d54841c379e4a8729e63297bd73fa5a1eb796a7
ferd <beowulf1416@gmail.com>
2026-01-04 22:30:08 +0800

    modify warehouse component. use country selector.

--- src/app/modules/inventory/components/warehouse/warehouse.ts
+++ src/app/modules/inventory/components/warehouse/warehouse.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { NotificationService } from '../../../../services/notification-service';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { WarehouseService } from '../../services/warehouse-service';
+import { CountrySelector } from '../../../country-selector/components/country-selector/country-selector';
+import { Country } from '../../../country-selector/classes/country';
 
 @Component({
   selector: 'app-warehouse',
@@ -16,17 +21,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatButtonModule,
     MatFormFieldModule,
     MatInputModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    CountrySelector
   ],
   templateUrl: './warehouse.html',
   styleUrl: './warehouse.css',
 })
 export class Warehouse {
 
+  @ViewChild("nav_end_inv") nav_end!: MatSidenav;
+
   component = {
     error: '',
     countries: [],
     form_warehouse: new FormGroup({
+      warehouse_id: new FormControl('', []),
       name: new FormControl('', []),
       description: new FormControl('', []),
       address: new FormGroup({
@@ -38,4 +48,26 @@ export class Warehouse {
       })
     })
   };
+
+
+  constructor(
+    private ns: NotificationService,
+    private ws: WarehouseService
+  ) {}
+
+  save(): void {
+    console.debug("save");
+  }
+
+  select_country(): void {
+    console.debug("select_country");
+    
+    this.nav_end?.open();
+  }
+
+  on_countries_selected(countries: Array<Country>): void {
+    console.debug("on_countries_selected", countries);
+    
+    this.nav_end?.close();
+  }
 }
