68a3975acb3ac33427878db771e3823ce7e9b9a8
ferd <beowulf1416@gmail.com>
2026-01-10 11:42:43 +0800

    switch to signals form

--- src/app/modules/inventory/components/warehouse/warehouse.ts
+++ src/app/modules/inventory/components/warehouse/warehouse.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, ViewChild } from '@angular/core';
+import { Component, signal, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -11,6 +11,8 @@ import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { WarehouseService } from '../../services/warehouse-service';
 import { CountrySelector } from '../../../country-selector/components/country-selector/country-selector';
 import { Country } from '../../../country-selector/classes/country';
+import { Uuid } from '../../../../classes/uuid';
+import { form } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-warehouse',
@@ -32,21 +34,34 @@ export class Warehouse {
 
   @ViewChild("nav_end_inv") nav_end!: MatSidenav;
 
+  warehouse = signal({
+    warehouse_id: Uuid.generate(),
+    name: "",
+    description: "",
+    address: {
+      street: "",
+      city: "",
+      zip: "",
+      country_id: -1
+    }
+  })
+
   component = {
     error: '',
-    countries: [],
-    form_warehouse: new FormGroup({
-      warehouse_id: new FormControl('', []),
-      name: new FormControl('', []),
-      description: new FormControl('', []),
-      address: new FormGroup({
-        street: new FormControl('', []),
-        city: new FormControl('', []),
-        state: new FormControl('', []),
-        zip: new FormControl('', []),
-        country_id: new FormControl('', [])
-      })
-    })
+    countries: new Array<Country>(),
+    // form_warehouse: new FormGroup({
+    //   warehouse_id: new FormControl('', []),
+    //   name: new FormControl('', []),
+    //   description: new FormControl('', []),
+    //   address: new FormGroup({
+    //     street: new FormControl('', []),
+    //     city: new FormControl('', []),
+    //     state: new FormControl('', []),
+    //     zip: new FormControl('', []),
+    //     country_id: new FormControl('', [])
+    //   })
+    // })
+    form_warehouse: form(this.warehouse)
   };
 
 
