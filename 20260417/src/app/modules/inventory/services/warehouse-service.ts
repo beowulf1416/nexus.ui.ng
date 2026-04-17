d39f5344f4d1c994003a79da6eea96491d8b8986
ferd <beowulf1416@gmail.com>
2026-04-17 21:51:40 +0800

    starting over

---
+++ 20260417/src/app/modules/inventory/services/warehouse-service.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Uuid } from "../../../classes/uuid";
+import { Observable } from "rxjs";
+import { ApiResponse } from "../../../classes/api-response";
+import { CONSTANTS } from "../../../classes/constants";
+import { INV_CONSTANTS } from "../classes/constants";
+import { Warehouse } from "../classes/warehouse";
+
+@Injectable({
+  providedIn: "root",
+})
+export class WarehouseService {
+  constructor(private http: HttpClient) {}
+
+  warehouse_save(
+    tenant_id: Uuid,
+    warehouse: Warehouse,
+  ): Observable<ApiResponse> {
+    console.debug("warehouse_save", warehouse);
+    console.debug(tenant_id instanceof Uuid);
+    console.debug(warehouse instanceof Warehouse);
+
+    const warehouse_id: Uuid = warehouse.id;
+    console.debug(warehouse_id instanceof Uuid);
+
+    return this.http.post<ApiResponse>(
+      CONSTANTS.api_base_url + INV_CONSTANTS.path_warehouse_save,
+      {
+        tenant_id: tenant_id.to_string(),
+        warehouse_id: warehouse.id.to_string(),
+        name: warehouse.name,
+        description: warehouse.description,
+        address: {
+          street: warehouse.address.street,
+          city: warehouse.address.city,
+          state: warehouse.address.state,
+          zip: warehouse.address.zip,
+          country_id: warehouse.address.country_id,
+        },
+      },
+    );
+  }
+}
