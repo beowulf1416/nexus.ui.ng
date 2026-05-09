29a0ef777964f7b49dd2ee48c6c56e8d8633dc3a
ferd <beowulf1416@gmail.com>
2026-05-09 10:53:00 +0800

    add user profile lib

---
+++ nexus/projects/user-profile/src/lib/user-profile.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-user-profile',
+  imports: [],
+  template: ` <p>user-profile works!</p> `,
+  styles: ``,
+})
+export class UserProfile {}
