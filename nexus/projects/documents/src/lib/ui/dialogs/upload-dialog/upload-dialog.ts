67f626fe7714a7ac127db1f0a4411c64f1dde1d8
ferd <beowulf1416@gmail.com>
2026-06-09 22:23:45 +0800

    modify documents ui

--- nexus/projects/documents/src/lib/ui/dialogs/upload-dialog/upload-dialog.ts
+++ nexus/projects/documents/src/lib/ui/dialogs/upload-dialog/upload-dialog.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { email, form, FormField, required, submit } from '@angular/forms/signals';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'lib-upload-dialog',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    FormField,
+  ],
   templateUrl: './upload-dialog.html',
   styleUrl: './upload-dialog.css',
 })
-export class UploadDialog {}
+export class UploadDialog {
+
+  constructor(
+    private dr: MatDialogRef<UploadDialog>
+  ) {}
+
+  on_upload(event: Event) {
+    this.dr.close();
+  }
+
+  on_cancel(event: Event) {
+    console.log("on_cancel");
+    this.dr.close();
+  }
+}
--- nexus/projects/documents/src/lib/ui/features/files/files.ts
+++ nexus/projects/documents/src/lib/ui/features/files/files.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from 'core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+// import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 
 import { Navigator } from '../../../ui/components/navigator/navigator';
+import { UploadDialog } from '../../dialogs/upload-dialog/upload-dialog';
 
 
 @Component({
@@ -10,9 +15,28 @@ import { Navigator } from '../../../ui/components/navigator/navigator';
   imports: [
     Header,
     Navigator,
+    MatIconModule,
+    MatButtonModule,
     MatToolbarModule,
   ],
   templateUrl: './files.html',
   styleUrl: './files.css',
 })
-export class Files {}
+export class Files {
+
+  constructor(
+    private md: MatDialog
+  ) {
+  }
+
+  on_upload(event: Event): void {
+    let dr = this.md.open(UploadDialog, {
+      position: {
+        right: '10px'
+      },
+    });
+    dr.afterClosed().subscribe((result: any) => {
+      console.debug(result);
+    });
+  }
+}
