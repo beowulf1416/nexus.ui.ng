82a5c023baef25f38ab2607d6b212460dd76c46e
ferd <beowulf1416@gmail.com>
2026-06-09 22:49:30 +0800

    modify admin tenants page

--- nexus/projects/admin-tenants/src/lib/ui/dialogs/tenant-dialog/tenant-dialog.ts
+++ nexus/projects/admin-tenants/src/lib/ui/dialogs/tenant-dialog/tenant-dialog.ts
@@ -1,9 +1,31 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'lib-tenant-dialog',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDialogModule,
+  ],
   templateUrl: './tenant-dialog.html',
   styleUrl: './tenant-dialog.css',
 })
-export class TenantDialog {}
+export class TenantDialog {
+
+  constructor(private dr: MatDialogRef<TenantDialog>) {}
+
+  on_cancel(event: Event) {
+    this.dr.close();
+  }
+
+  on_submit(event: Event) {
+    this.dr.close();
+  }
+}
--- nexus/projects/admin-tenants/src/lib/ui/features/tenants/tenants.ts
+++ nexus/projects/admin-tenants/src/lib/ui/features/tenants/tenants.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { Header, ApiResponse } from 'core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialog } from '@angular/material/dialog';
+
+import { TenantDialog } from '../../dialogs/tenant-dialog/tenant-dialog';
 
 @Component({
   selector: 'lib-tenants',
-  imports: [],
+  imports: [
+    Header,
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+  ],
   templateUrl: './tenants.html',
   styleUrl: './tenants.css',
 })
-export class Tenants {}
+export class Tenants {
+
+
+  constructor(
+    private md: MatDialog) {
+
+  }
+
+  show_tenant_dialog(event: Event) {
+    let dr = this.md.open(TenantDialog, {
+      position: {
+        right: '10px'
+      }
+    })
+    dr.afterClosed().subscribe((result: any) => {
+      console.debug(result);
+    })
+  }
+}
