6bc7bbb4db020f8991d96fdc422bf8c196958c26
ferd <beowulf1416@gmail.com>
2026-06-14 09:47:35 +0800

    modify tenant service

--- nexus/projects/admin-tenants/src/lib/constants.ts
+++ nexus/projects/admin-tenants/src/lib/constants.ts
@@ -1,11 +1,7 @@
 export const URLS = {
   base_url: 'http://localhost:8080',
 
-  tenant_save: '/admin/tenants/save',
-  fetch_tenant: '/admin/tenants/fetch/id',
   fetch_tenants: '/admin/tenants/fetch',
 
-  role_save: '/admin/tenants/role/save',
-
-  fetch_users: '/admin/tenants/fetch/users'
-}
+  fetch_roles: '/admin/tenants/roles/fetch',
+};
--- nexus/projects/admin-tenants/src/lib/services/tenants-service.ts
+++ nexus/projects/admin-tenants/src/lib/services/tenants-service.ts
@@ -1,6 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, pipe, map, catchError } from 'rxjs';
+
+import { ApiResponse } from 'core';
+import { URLS } from '../constants';
+import { TenantItem } from '../models/tenant-item';
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class TenantsService {}
+export class TenantsService {
+
+  constructor(
+    private http: HttpClient,
+  ) {
+
+  }
+
+  fetch_tenants(filter: string): Observable<Array<TenantItem>> {
+    return this.http.post<ApiResponse>(
+      `${URLS.base_url}${URLS.fetch_tenants}`,
+      {
+        filter: filter
+      }).pipe(
+        map((r: ApiResponse) => {
+          const tenants = (r.data as {
+            tenants: Array<TenantItem>
+          })
+          return tenants.tenants;
+        }),
+        catchError((error) => {
+          console.error(error);
+          throw new Error(error);
+        }),
+      );
+  }
+}
