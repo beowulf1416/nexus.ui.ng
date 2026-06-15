0389aeebfac372d4d84262bdc6c6b21a8759ecda
ferd <beowulf1416@gmail.com>
2026-06-15 16:56:41 +0800

    add placeholder libs

---
+++ nexus/projects/pos/src/lib/pos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-pos',
+  imports: [],
+  template: ` <p>pos works!</p> `,
+  styles: ``,
+})
+export class Pos {}
---
+++ nexus/projects/pos/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of pos
+ */
+
+export * from './lib/pos';
