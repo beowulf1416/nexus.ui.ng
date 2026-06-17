ec5d30649e6152575862f5dfa17a9185e2e0f0e6
ferd <beowulf1416@gmail.com>
2026-06-17 09:50:00 +0800

    modify assign roles dialog

--- nexus/projects/admin-tenants/src/lib/ui/dialogs/assign-roles-dialog/assign-roles-dialog.ts
+++ nexus/projects/admin-tenants/src/lib/ui/dialogs/assign-roles-dialog/assign-roles-dialog.ts
@@ -1,9 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+
+class RoleItemRole {
+  constructor(
+    readonly role: Role,
+    readonly selected: boolean = false) {}
+}
 
 @Component({
   selector: 'lib-assign-roles-dialog',
-  imports: [],
+  imports: [
+    MatIconModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatDialogModule,
+  ],
   templateUrl: './assign-roles-dialog.html',
   styleUrl: './assign-roles-dialog.css',
 })
-export class AssignRolesDialog {}
+export class AssignRolesDialog {
+
+  model = signal({
+    filter: '',
+    selected: new Array<RoleItemRole>(),
+    matches: new Array<RoleItemRole>()
+  });
+
+  component = {
+    errors: signal(new Array<String>()),
+    form: form(this.model, (f) => {
+      required(f.filter, { message: 'Filter is required' })
+    })
+  };
+
+
+  constructor() {}
+}
