7182979c1a0336030c9c5831b4830634e6ecb0c6
ferd <beowulf1416@gmail.com>
2026-06-21 13:31:00 +0800

    modify notification service

--- nexus/projects/core/src/lib/services/notification-service.ts
+++ nexus/projects/core/src/lib/services/notification-service.ts
@@ -1,16 +1,34 @@
-import { Service } from '@angular/core';
+import { Service, Signal, signal } from '@angular/core';
+
+
+export class Notification {
+  constructor(
+    readonly type: 'info' | 'error',
+    readonly message: string,
+  ) {}
+}
 
 @Service()
 export class NotificationService {
-
-
   private _notifications = signal(new Array<Notification>())
-
-  public notifications = computed(() => this._notifications());
 
   constructor() {}
 
-  add_info(message: string): void {
+  get notifications(): Signal<Array<Notification>> {
+    return this._notifications.asReadonly();
+  }
 
+  info(message: string): void {
+    console.info(message);
+    this._notifications.update((notifications) => {
+      return notifications.concat(new Notification('info', message));
+    });
+  }
+
+  error(message: string): void {
+    console.error(message);
+    this._notifications.update((notifications) => {
+      return notifications.concat(new Notification('error', message));
+    });
   }
 }
--- nexus/projects/core/src/public-api.ts
+++ nexus/projects/core/src/public-api.ts
@@ -10,5 +10,6 @@ export * from './lib/models/api-response';
 export * from './lib/models/uuid';
 export * from './lib/models/user';
 export * from './lib/models/tenant';
+export * from './lib/services/notification-service';
 export * from './lib/services/user-service';
 export * from './lib/ui/components/app-header/app-header';
