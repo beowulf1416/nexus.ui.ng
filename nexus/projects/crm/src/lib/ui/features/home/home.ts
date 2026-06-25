6e7d7e4c7107e95e030baf5ccb584cf44b859a8a
ferd <beowulf1416@gmail.com>
2026-06-25 22:38:19 +0800

    placeholder pages for crm

--- nexus/projects/crm/src/lib/ui/features/home/home.ts
+++ nexus/projects/crm/src/lib/ui/features/home/home.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { AppHeader } from 'core';
+import { SidebarLeft } from '../../components/sidebar-left/sidebar-left';
+
 
 @Component({
   selector: 'lib-home',
-  imports: [],
+  imports: [
+    RouterOutlet,
+    MatIconModule,
+    AppHeader,
+    SidebarLeft,
+    MatSidenavModule
+  ],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
