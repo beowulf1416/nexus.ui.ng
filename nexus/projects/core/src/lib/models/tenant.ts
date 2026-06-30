4b794f71e30766ee5765f106f4efe331bac9c39a
ferd <beowulf1416@gmail.com>
2026-06-30 22:10:58 +0800

    modify tenants format

--- nexus/projects/core/src/lib/models/tenant.ts
+++ nexus/projects/core/src/lib/models/tenant.ts
@@ -3,7 +3,8 @@ import { Uuid } from './uuid';
 export class Tenant {
   constructor(
     readonly id: Uuid,
-    readonly name: string) {}
+    readonly name: string
+  ) {}
 
   static default(): Tenant {
     return new Tenant(
