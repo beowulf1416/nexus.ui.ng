e04d74f271df621be6f1c5411d9b095f1501f829
ferd tomale <beowulf1416@gmail.com>
2026-07-05 17:01:53 +0800

    add content-security-policy header

--- nexus/projects/nexus/src/main.ts
+++ nexus/projects/nexus/src/main.ts
@@ -2,4 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+if (
+  typeof window !== 'undefined' &&
+  (window as any).trustedTypes &&
+  (window as any).trustedTypes.createPolicy
+) {
+  (window as any).trustedTypes.createPolicy('angular', {
+    createHTML: (html: string) => html,
+    createScriptURL: (url: string) => url,
+    createScript: (script: string) => script,
+    createStyleURL: (url: string) => url,
+  });
+}
+
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
