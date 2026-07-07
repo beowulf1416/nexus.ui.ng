b9c7724ee5d0d25967dfaff71e6915c4c8af5dd6
ferd tomale <beowulf1416@gmail.com>
2026-07-07 22:01:13 +0800

    remove account dialog

--- nexus/projects/accounting/src/lib/ui/dialogs/account/account.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-account',
-  imports: [],
-  templateUrl: './account.html',
-  styleUrl: './account.css',
-})
-export class Account {}
